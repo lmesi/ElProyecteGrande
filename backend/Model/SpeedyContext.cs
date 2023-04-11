a6038be16eba3e9060825dbb997343751e2215a3
lmesi <lszl.emese@gmail.com>
2023-04-11 15:31:45 +0200

    feat: initialize database

---
+++ backend/Model/SpeedyContext.cs
@@ -0,0 +1,21 @@
+using Backend.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Model;
+
+public class SpeedyContext : DbContext
+{
+    public SpeedyContext(DbContextOptions<SpeedyContext> options) : base(options)
+    {
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(@"DataSource=db/database.db;");
+    }
+
+    public DbSet<Admin> Admins { get; set; }
+    public DbSet<Driver> Drivers { get; set; }
+    public DbSet<Company> Companies { get; set; }
+    public DbSet<Order> Orders { get; set; }
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,13 +1,21 @@
+using Backend.Data;
+using Backend.Model;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddDbContext<SpeedyContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConfiguration")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,5 +29,25 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+CreateDbIfNotExists(app);
 app.Run();
+
+static void CreateDbIfNotExists(WebApplication application)
+{
+    Console.WriteLine("init INIT init INIT");
+    using (var scope = application.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        try
+        {
+            Console.WriteLine("TRY");
+            var context = services.GetRequiredService<SpeedyContext>();
+            DbInitializer.Initialize(context);
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occurred creating the DB.");
+        }
+    }
+}
