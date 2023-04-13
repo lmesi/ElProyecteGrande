638e7b2521b732f0d14c56a9b266d1e73c01877f
bmarci888 <bravikmarci@gmail.com>
2023-04-13 13:46:04 +0200

    fix: change content result to status code

--- backend/Controllers/CompanyController.cs
+++ backend/Controllers/CompanyController.cs
@@ -21,27 +21,27 @@ namespace Backend.Controllers;
         public async Task<IActionResult> GetAllCompanies()
         {
             var companies= await _companyService.GetAllCompanies();
-            return companies.Count == 0 ? NotFound() : Ok(companies);
+            return Ok(companies);
         }
         
         [HttpPost]
         public async Task<IActionResult> AddCompany([FromBody] CompanyDto companyDto)
         {
             var success = await _companyService.AddCompany(companyDto);
-            return success ? Ok("New company added") : Content(ErrorMessage);
+            return success ? Ok("New company added") : StatusCode(400, ErrorMessage);
         }
         
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateCompany([FromBody] CompanyDto companyDto, long id)
         {
             var success = await _companyService.UpdateCompany(companyDto, id);
-            return success ? Ok("Company updated") : Content(ErrorMessage);
+            return success ? Ok("Company updated") : StatusCode(400, ErrorMessage);
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteCompany(long id)
         {
             var success = await _companyService.DeleteCompany(id);
-            return success ? Ok("Company deleted") : Content(ErrorMessage);
+            return success ? Ok("Company deleted") : StatusCode(400, ErrorMessage);
         }
     }
