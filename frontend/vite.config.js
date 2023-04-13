b8b84648f614388c0fdee2c006060bee7cbcf7ae
lmesi <41645404+lmesi@users.noreply.github.com>
2023-04-13 10:14:25 +0200

    feat: add server proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://localhost:7200",
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 })
