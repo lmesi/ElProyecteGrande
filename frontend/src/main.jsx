c62cdc7fd1d6d30bf2b9e7a74f46ea519fcbda8a
Citrom333 <citrom333@gmail.com>
2023-04-24 13:13:07 +0200

    feat: create routing for admin page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import AdminPage from './AdminPage'
 import App from './App'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 
-const router = createBrowserRouter([{
-  path: "/",
-  element: <App />,
-  children:
-    [
-      {
-        path: "",
-        element: <App />
-      },
-    ]
-}])
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/admin",
+    element: <AdminPage />
+  },
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
