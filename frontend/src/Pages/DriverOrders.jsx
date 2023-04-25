8d8b85182c09a62141e21074cda72a907df10e99
lmesi <lszl.emese@gmail.com>
2023-04-25 13:28:55 +0200

    feat(driverOrders): create 'add unloading date' button function

--- frontend/src/Pages/DriverOrders.jsx
+++ frontend/src/Pages/DriverOrders.jsx
@@ -5,15 +5,38 @@ const fetchDriverOrders = (id) => {
   return fetch(`/api/Driver/${id}`).then((res) => res.json());
 };
 
+const updateUnloadingDate = (orderId, date) => {
+  return fetch(`/api/Orders/${orderId}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ unloadingDate: date }),
+  });
+};
+
 const DriverOrders = () => {
   const { id } = useParams();
   const [orders, setOrders] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [orderToShow, setOrderToShow] = useState();
 
   useEffect(() => {
     fetchDriverOrders(id).then((data) => {
       setOrders(data);
     });
   }, []);
+
+  const handleAddDate = (orderId, date) => {
+    updateUnloadingDate(orderId, date)
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => {
+        fetchDriverOrders(id).then((data) => {
+          setOrders(data);
+          setShowModal(false);
+        });
+      });
+  };
 
   if (orders === null) return <h1>Loading...</h1>;
 
@@ -42,12 +65,46 @@ const DriverOrders = () => {
               <td>{order.unloadingDate?.split("T")[0] ?? "uncompleted"}</td>
               <td>{order.goodsName}</td>
               <td>
-                <button>Add unloading date</button>
+                <button
+                  onClick={() => {
+                    setShowModal(true);
+                    setOrderToShow(order);
+                  }}
+                >
+                  Add unloading date
+                </button>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
+      {showModal && (
+        <AddDateModal
+          isOpen={setShowModal}
+          onUpdate={handleAddDate}
+          orderId={orderToShow.id}
+          defaultDate={orderToShow.unloadingDate ?? new Date().toJSON()}
+        />
+      )}
+    </div>
+  );
+};
+
+const AddDateModal = ({ isOpen, onUpdate, orderId, defaultDate }) => {
+  const [date, setDate] = useState(defaultDate.split("T")[0]);
+
+  return (
+    <div className="blackout">
+      <div className="modal">
+        <h2>Add unloading date</h2>
+        <input
+          type="date"
+          value={date}
+          onChange={(event) => setDate(event.target.value)}
+        />
+        <button onClick={() => onUpdate(orderId, date)}>Save</button>
+        <button onClick={() => isOpen(false)}>Cancel</button>
+      </div>
     </div>
   );
 };
