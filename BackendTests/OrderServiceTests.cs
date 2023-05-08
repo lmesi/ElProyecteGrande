c713d76caadc19acc81e8ac825f10faf1a6eb7ec
lmesi <lszl.emese@gmail.com>
2023-05-08 21:14:47 +0200

    test(order): add missing order service tests

--- BackendTests/OrderServiceTests.cs
+++ BackendTests/OrderServiceTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Backend.Model;
@@ -11,6 +12,33 @@ namespace BackendTests;
 
 public class Tests
 {
+    private static readonly OrderDto OrderDto1 = new()
+    {
+        CompanyId = 1,
+        LoadingAddress = "loadingAddress",
+        UnloadingAddress = "unloadingAddress",
+        GoodsId = 1,
+        UserId = 1,
+    };
+
+    private static readonly OrderDto OrderDto2 = new()
+    {
+        CompanyId = 2,
+        LoadingAddress = "loadingAddress2",
+        UnloadingAddress = "unloadingAddress2",
+        GoodsId = 2,
+        UserId = 2,
+    };
+    
+    private static readonly OrderDto orderDto3 = new()
+    {
+        CompanyId = 2,
+        LoadingAddress = "loadingAddress3",
+        UnloadingAddress = "unloadingAddress3",
+        GoodsId = 1,
+        UserId = 1,
+    };
+
     [SetUp]
     public void Setup()
     {
@@ -64,18 +92,10 @@ public class Tests
     [Test]
     public async Task AddOrderTest()
     {
-        var orderDto = new OrderDto
-        {
-            CompanyId = 1,
-            LoadingAddress = "loadingAddress",
-            UnloadingAddress = "unloadingAddress",
-            GoodsId = 1,
-            UserId = 1,
-        };
-        
         var db = GetMemoryContext();
         var service = new OrderService(db);
-        var result = await service.AddOrder(orderDto);
+        
+        var result = await service.AddOrder(OrderDto1);
         
         Assert.True(result);
     }
@@ -83,17 +103,17 @@ public class Tests
     [Test]
     public async Task AddOrderFailsOnMissingCompanyIdTest()
     {
-        var orderDto = new OrderDto
+        var wrongOrderDto = new OrderDto
         {
             LoadingAddress = "loadingAddress",
             UnloadingAddress = "unloadingAddress",
             GoodsId = 1,
             UserId = 1,
         };
-        
         var db = GetMemoryContext();
         var service = new OrderService(db);
-        var result = await service.AddOrder(orderDto);
+        
+        var result = await service.AddOrder(wrongOrderDto);
         
         Assert.False(result);
     }
@@ -101,60 +121,118 @@ public class Tests
     [Test]
     public async Task GetOrderByIdTest()
     {
-        var orderDto = new OrderDto
-        {
-            CompanyId = 1,
-            LoadingAddress = "loadingAddress",
-            UnloadingAddress = "unloadingAddress",
-            GoodsId = 1,
-            UserId = 1,
-        };
         var db = GetMemoryContext();
         var service = new OrderService(db);
-        await service.AddOrder(orderDto);
+        await service.AddOrder(OrderDto1);
+        
         var result = await service.GetOrder(1);
         
-        Assert.That(result.LoadingAddress, Is.EqualTo(orderDto.LoadingAddress));
+        Assert.That(result.LoadingAddress, Is.EqualTo(OrderDto1.LoadingAddress));
+    }
+
+    [Test]
+    public async Task GetOrderByIdWithWrongIdTest()
+    {
+        var db = GetMemoryContext();
+        var service = new OrderService(db);
+        var result = await service.GetOrder(1);
+        
+        //result should be null instead of new OrderToShow()
+        //Assert.That(result, Is.Null);
+        Assert.That(result.Id, Is.EqualTo(0));
     }
 
     [Test]
     public async Task GetAllOrdersTest()
     {
-        var orderDto1 = new OrderDto
-        {
-            CompanyId = 1,
-            LoadingAddress = "loadingAddress1",
-            UnloadingAddress = "unloadingAddress1",
-            GoodsId = 1,
-            UserId = 1,
-        };
-        
-        var orderDto2 = new OrderDto
-        {
-            CompanyId = 2,
-            LoadingAddress = "loadingAddress2",
-            UnloadingAddress = "unloadingAddress2",
-            GoodsId = 2,
-            UserId = 2,
-        };
-        
-        var orderDto3 = new OrderDto
-        {
-            CompanyId = 2,
-            LoadingAddress = "loadingAddress3",
-            UnloadingAddress = "unloadingAddress3",
-            GoodsId = 1,
-            UserId = 1,
-        };
-        
         var db = GetMemoryContext();
         var service = new OrderService(db);
-        await service.AddOrder(orderDto1);
-        await service.AddOrder(orderDto2);
+        await service.AddOrder(OrderDto1);
+        await service.AddOrder(OrderDto2);
         await service.AddOrder(orderDto3);
+        
         var result = await service.GetAllOrders();
         
         Assert.That(result.Count, Is.EqualTo(3));
+    }
+
+    [Test]
+    public async Task UpdateOrderCanUpdateUnloadingDateTest()
+    {
+        var newOrderDto = new OrderDto
+        {
+            UnloadingDate = new DateTime(2023,05,08)
+        };
+        var db = GetMemoryContext();
+        var service = new OrderService(db);
+        await service.AddOrder(OrderDto1);
+        var resultValue = await service.UpdateOrder(1, newOrderDto);
+
+        var result = await service.GetOrder(1);
+        
+        Assert.That(result.UnloadingDate, Is.EqualTo(newOrderDto.UnloadingDate));
+        Assert.True(resultValue);
+    }
+
+    [Test]
+    public async Task UpdateOrderFailsOnNonExistentOrder()
+    {
+        var db = GetMemoryContext();
+        var service = new OrderService(db);
+        
+        var result = await service.UpdateOrder(1, new OrderDto());
+        
+        Assert.False(result);
+    }
+
+    [Test]
+    public async Task DeleteOrderTest()
+    {
+        var db = GetMemoryContext();
+        var service = new OrderService(db);
+        await service.AddOrder(OrderDto1);
+        var orderCountBeforeDelete = (await service.GetAllOrders()).Count;
+        var returnValue = await service.DeleteOrder(1);
+
+        var orderCountAfterDelete = (await service.GetAllOrders()).Count;
+        
+        Assert.That(orderCountAfterDelete + 1, Is.EqualTo(orderCountBeforeDelete));
+        Assert.True(returnValue);
+    }
+    
+    [Test]
+    public async Task DeleteOrderReturnsFalseOnNonExistentIdTest()
+    {
+        var db = GetMemoryContext();
+        var service = new OrderService(db);
+
+        var result = await service.DeleteOrder(1);
+        
+        Assert.False(result);
+    }
+
+    [Test]
+    public async Task GetOrdersByUserId()
+    {
+        var db = GetMemoryContext();
+        var service = new OrderService(db);
+        await service.AddOrder(OrderDto1);
+        await service.AddOrder(OrderDto2);
+
+        var result = await service.GetOrdersByUserId(1);
+        
+        Assert.That(result.Count, Is.EqualTo(1));
+    }
+
+    [Test]
+    public async Task GetOrdersByUserIdReturnsEmptyListOnWrongIdTest()
+    {
+        var db = GetMemoryContext();
+        var service = new OrderService(db);
+
+        var result = await service.GetOrdersByUserId(3);
+        
+        Assert.That(result.Count, Is.EqualTo(0));
     }
     
     private SpeedyContext GetMemoryContext()
